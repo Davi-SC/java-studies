be684ab0787afdfdf1d6b8ffef016fd4e60da9d7
Davi-SC <cruzdavi001@gmail.com>
2024-10-11 21:21:07 -0300

    First commit

---
+++ src/aula01/Cachorro.java
@@ -0,0 +1,24 @@
+package aula01;
+
+public class Cachorro {
+    //ATRIBUTOS
+
+    String raca;
+
+    String nome;
+
+    String dono;
+
+    int idade;
+
+    //METODOS
+
+    void latir(){
+        System.out.println("AU AU");
+    }
+
+    void morder(){
+        System.out.println("CREU!!");
+    }
+
+}
---
+++ src/aula01/MyFirstProgramJava.java
@@ -0,0 +1,12 @@
+package aula01;
+
+public class MyFirstProgramJava {
+    public static void main(String[] args) {
+        Cachorro pet = new Cachorro();
+        pet.dono = "CW";
+        pet.nome = "LARVA";
+        pet.raca = "Caramelo";
+        pet.idade = 454;
+        
+    }
+}
