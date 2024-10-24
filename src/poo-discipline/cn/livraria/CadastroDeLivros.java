c32d7697d6e0204fe8860159e73b02b844ef724d
Davi-SC <cruzdavi001@gmail.com>
2024-10-24 19:55:07 -0300

    Aula e POO 24/10/2024, definindo classes de uma livraria

---
+++ src/poo-discipline/cn/livraria/CadastroDeLivros.java
@@ -0,0 +1,16 @@
+package cn.livraria;
+
+public class CadastroDeLivros {
+
+    Livro[] livros = new Livro[]{new Livro()};
+
+    public void setLivros(Livro[] livros) {
+        this.livros = livros;
+    }
+
+    public Livro[] getLivros() {
+        return livros;
+    }
+    
+    public void
+}
---
+++ src/poo-discipline/cn/livraria/Livro.java
@@ -0,0 +1,13 @@
+package cn.livraria;
+
+public class Livro {
+
+    String titulo;
+    String descricao;
+    double valor;
+    String isbn;
+
+    public void mostrarDetalhes(){
+
+            }
+}
