9952d687081fab8243362d274a37672620828f4a
Davi-SC <cruzdavi001@gmail.com>
2024-10-25 21:31:00 -0300

    Continuando exercicios de OO

--- src/poo-discipline/lesson02/cn/livraria/CadastroDeLivros.java
+++ src/poo-discipline/lesson02/cn/livraria/CadastroDeLivros.java
@@ -6,7 +6,7 @@ public class CadastroDeLivros {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
         Autor autor = new Autor("Davi", "davi@gmail.com", "123.456.789-00");
         Livro livroJava = new Livro("Java","Aprenda java", "103-121-3-12", 75.99 ,autor);
         Livro livroPython = new Livro("Python","Aprenda Python", "102-121-2-45", 99.99 , autor);
--- src/poo-discipline/lesson02/cn/livraria/Livro.java
+++ src/poo-discipline/lesson02/cn/livraria/Livro.java
@@ -31,4 +31,30 @@ public class Livro {
 
     }
 
+    //SETS
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+    //GETS
+    public String getNome() {
+        return nome;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public String getIsbn() {
+        return isbn;
+    }
+    public double getValor() {
+        return valor;
+    }
 }
